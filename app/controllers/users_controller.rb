1de3387ff173d796741f8c1f3c595c85c1d59fc1
ha-manel <manelhammouche3@gmail.com>
2022-09-22 10:58:22 +0100

    Add register functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def register
+    if User.find_by(username: params[:username].downcase)
+      render json: { error: 'Username already exists! please choose another one.' }
+    else
+      @user = User.new(user_params)
+      @user.username = @user.username.downcase
+      if @user.save
+        render json: { user: @user, logged_in: true }
+      else
+        render json: { error: 'There was an error, please try again!' }
+      end
+    end
+  end
+
+  def login; end
+
+  private
+
+  def user_params
+    params.permit(:name, :username, :email)
+  end
+end
