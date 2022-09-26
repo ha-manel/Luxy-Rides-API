e1759e5e280118594d7b03e881f19b5710b51b34
ha-manel <manelhammouche3@gmail.com>
2022-09-26 09:55:42 +0100

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   post 'api/v1/register/:username/:name/:email', to: 'users#register'
   get 'api/v1/login/:username', to: 'users#login'
+
+  get 'api/v1/cars', to: 'cars#index'
+  post 'api/v1/car/:user_id/:model/:picture/:driver_name', to: 'cars#create'
+  delete 'api/v1/car/:id', to: 'cars#delete'
 end
