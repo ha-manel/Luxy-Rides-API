5a0120aa2fa7cf597a4154f07b977822d1270fea
sarazaToussaint <tmsaraza@gmail.com>
2022-10-04 21:38:30 +0200

    Fix linter errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
   describe 'POST register' do
     it 'If the username is not exists in db returns status 201 (created)' do
       post :register,
@@ -10,10 +9,9 @@ RSpec.describe UsersController, type: :controller do
     end
 
     it 'returns status 500 (internal_server_error) ' do
-      post :register, 
-           params: { name: 'Test', username: ' ', email: 'test@gmail.com'  }
+      post :register,
+           params: { name: 'Test', username: ' ', email: 'test@gmail.com' }
       expect(response.status).to eq(500)
     end
-
   end
 end
