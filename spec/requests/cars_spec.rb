4bd4b28ea6b0018c4859974cd04e48caba3aeebf
HammadSiddique <hammad.siddique01@gmail.com>
2022-10-04 19:54:13 +0500

    Car controller requests tests

--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe 'Cars', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe CarsController, type: :controller do
+  before :all do
+    @user = User.create(email: 'testing@test.com', name: 'Testing', username: 'testing')
+    @car = Car.create(model: 'BMW', picture: 'picture link', driver_name: 'test', price: 250, user_id: @user.id)
   end
+
+  describe 'POST create' do
+    it 'returns status 201 (created)' do
+      post :create, params: { user_id: @user.id, model: 'BMW', picture: 'picture link', driver_name: 'test', price: 250}
+      expect(response.status).to eq(201)
+    end
+
+    it 'returns status 500 (internal_server_error) ' do
+      post :create, params: { user_id: @user.id, model: 'BMW', picture: 'picture link', driver_name: 'test', price: -250}
+      expect(response.status).to eq(500)
+    end
+  end
+
 end
