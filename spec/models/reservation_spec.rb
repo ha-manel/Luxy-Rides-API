b2a3604023e7082ef698adcb5d9ebb695f88aa70
ha-manel <manelhammouche3@gmail.com>
2022-10-04 13:53:58 +0100

    Add reservation tests

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    @user = User.create(email: 'test@test.com', name: 'Test', username: 'test')
+    @car = Car.create(model: 'BMW', picture: 'picture link', driver_name: 'test', price: 250, user_id: @user.id)
+    @reservation = Reservation.new(user_id: @user.id, car_id: @car.id, date: '2022-09-04', city: 'test')
+  end
+
+  it 'is valid' do
+    expect(@reservation).to be_valid
+  end
+
+  it 'date should be present' do
+    @reservation.date = nil
+    expect(@reservation).to_not be_valid
+  end
+
+  it 'city should be present' do
+    @reservation.city = nil
+    expect(@reservation).to_not be_valid
+  end
 end
